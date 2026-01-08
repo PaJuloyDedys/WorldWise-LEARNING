82d73320d7fb7dd8a5df426557277505b7db94c4
Misha <noytbyk@gmail.com>
2026-01-08 18:26:40 +0200

    project without styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-function App() {
-  const x = 21;
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
 
-  return <div>WorldWise</div>;
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
